9bec2a422bbe6e9485bbacdbd8236ff87578ec7a
TreJon House <houset@msoe.edu>
2016-10-10 08:18:15 -0500

    setting up karma/jasmine testing framework and travis cli

--- src/client/app/scripts/controllers/registeruser.js
+++ src/client/app/scripts/controllers/registeruser.js
@@ -17,25 +17,24 @@ angular.module('clientApp')
       password: ""
     };
     $scope.submit = function() {
-      authentication.register(registerUser.credentials).then(function() {
-        $location.path("/profile");
-      }, function errorCallback() {
-        console.log("error saving to db");
-      });
+      if (completedFields(registerUser.credentials)) {
+        authentication.register(registerUser.credentials).then(function() {
+          $location.path("/profile");
+        }, function errorCallback() {
+          console.log("error saving to db");
+        });
+      }
     };
   });
 
 
-function isDataComplete(formData) { //jshint ignore:line
-  var fName = formData.users.fullname;
-  var uName = formData.users.username;
-  var password = formData.users.password;
+function completedFields(credentials) { //jshint ignore:line
+  var fName = credentials.name;
+  var uName = credentials.username;
+  var password = credentials.password;
   fName = fName.trim();
   uName = uName.trim();
   password = password.trim();
-  console.log(fName);
-  console.log(uName);
-  console.log(password);
   if (!fName || fName.length === 0 || !uName || uName.length === 0)
     return false; //jshint ignore:line
   return true;
