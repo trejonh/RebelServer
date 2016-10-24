68d30faed23398b6ebefc14458879787a59073c5
TreJon House <houset@msoe.edu>
2016-10-24 13:26:15 -0500

    anoter test

--- src/client/app/Common/CommTestService.js
+++ src/client/app/Common/CommTestService.js
@@ -6,20 +6,15 @@
     .module("clientApp")
     .service("particleServ", particleServ);
 
-  particleServ.$inject = ["$http","$https"];
+  particleServ.$inject = ["$http"];
 
   function particleServ($http,$https) {//jshint ignore:line
     var getDeviceStatus = function() {
       return $http.get('http://'+window.location.hostname+':3000/particleTest');
     };
 
-    var toggle = function(onOff,id,token){
-      $https.post("https://api.particle.io/v1/devices/"+id+"/led?access_token="+token);
-    };
-
   return {
-    getDeviceStatus: getDeviceStatus,
-    toggle: toggle
-  };
+    getDeviceStatus: getDeviceStatus
+  }
 }
 })();
