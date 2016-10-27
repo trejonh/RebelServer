c093c3a04bc4419e9f44d791484ab941a1384a26
TreJon House <houset@msoe.edu>
2016-10-27 17:51:01 -0500

    reactive again

--- src/client/app/scripts/controllers/particleTestCtrl.js
+++ src/client/app/scripts/controllers/particleTestCtrl.js
@@ -12,17 +12,24 @@ angular.module('clientApp')
     var particle = this;
     particle.message = {};
     particle.url="";
-    //$timeout(function () {
+    particleServ.getDeviceStatus()
+      .success(function(data){
+        particle.message = data;
+        particle.url = "https://api.particle.io/v1/devices/"+data.deviceID+"/led?access_token="+data.accessToken;
+      })
+      .error(function(err){
+        console.log(err);
+      });
+      $scope.trustSrc = function(src){
+        return $sce.trustAsResourceUrl(src);
+      };
+    $timeout(function () {
       particleServ.getDeviceStatus()
         .success(function(data){
-          particle.message = data;
-          particle.url = "https://api.particle.io/v1/devices/"+data.deviceID+"/led?access_token="+data.accessToken;
+          $scope.message = data.message;
         })
         .error(function(err){
           console.log(err);
         });
-        $scope.trustSrc = function(src){
-          return $sce.trustAsResourceUrl(src);
-        };
-  //  }, 50000);
+    }, 50000);
   });
