42f04877fe17f146b97307fd12496ac9a4b2a9f0
TreJon House <houset@msoe.edu>
2016-12-05 19:26:47 -0600

    pasword strength and json parsing

--- src/server/Controllers/commTestController.js
+++ src/server/Controllers/commTestController.js
@@ -1,66 +1,101 @@
   /*
-Will be used only for submodule testing not for dev
-*/
-var mongoose = require("mongoose");
-var Message = mongoose.model("testMessagesModel");
-module.exports.setMessage = function(req, res) {
-    console.log(req.body.data);
-    JSON.parse(req.body.data,function(key,val){
-      console.log(key + " key");
-      console.log(val+" val");
-    });
-    /*
-    var mess = new Message();
-    mess.message = req.body.message;
-    mess.deviceID = req.body.deviceID;
-    mess.accessToken = req.body.accessToken;
-    mess.setDate();
-    Message.findOneAndUpdate({
-        "deviceID": mess.deviceID
-    }, {$set:{
-      "message": mess.message,
-      "accessToken": mess.accessToken
-    }}, {
-        upsert: true
-    }, function(err, doc) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.status(200);
-    });*/
-    res.status(200);
-};
+  Will be used only for submodule testing not for dev
+  */
+  var mongoose = require("mongoose");
+  var Message = mongoose.model("testMessagesModel");
+  module.exports.setMessage = function(req, res) {
+      var data = req.body.data;
+      var outlet = {
+          deviceID: 0,
+          outletNumber: 0,
+          nickname: "",
+          isOn: false,
+          wattage: 0,
+          timeSetOn: 0,
+          timeSetOff: 0,
+          elapsedTimeOn: 0
+      };
+      data = data.split(",");
+      for (var i = 0; i < data.length; i++) {
+          var outletData = data.split(":");
+          outletData = outletData[1];
+          switch (i) {
+              case 0:
+                  outlet.deviceID = parseInt(outletData);
+                  break;
+              case 1:
+                  outlet.outletNumber = parseInt(outletData);
+                  break;
+              case 2:
+                  outlet.nickname = parseInt(outletData);
+                  break;
+              case 3:
+                  outlet.isOn = parseInt(outletData);
+                  break;
+              case 4:
+                  outlet.timeSetOn = parseInt(outletData);
+                  break;
+              case 5:
+                  outlet.timeSetOff = parseInt(outletData);
+                  break;
+              case 6:
+                  outlet.elapsedTimeOn = parseInt(outletData);
+                  break;
+          }
+      }
+      console.log(outlet);
+      /*
+      var mess = new Message();
+      mess.message = req.body.message;
+      mess.deviceID = req.body.deviceID;
+      mess.accessToken = req.body.accessToken;
+      mess.setDate();
+      Message.findOneAndUpdate({
+          "deviceID": mess.deviceID
+      }, {$set:{
+        "message": mess.message,
+        "accessToken": mess.accessToken
+      }}, {
+          upsert: true
+      }, function(err, doc) {
+          if (err) {
+              console.log(err);
+              return;
+          }
+          res.status(200);
+      });*/
+      res.status(200);
+  };
 
-module.exports.readMessage = function(req, res) {
-    Message
-        .findOne({})
-        .populate("mess").exec(function(err, doc) {
-            if (err) {
-                console.log(err);
-                res.status(400);
-                return;
-            }
-            if (doc === null) {
-                console.log("doc is null");
-                var emptyDoc = {};
-                //just for testing
-                res.status(200).json(emptyDoc);
-                return;
-            }
-            console.log(doc);
-            res.status(200).json(doc);
-        });
-};
+  module.exports.readMessage = function(req, res) {
+      Message
+          .findOne({})
+          .populate("mess").exec(function(err, doc) {
+              if (err) {
+                  console.log(err);
+                  res.status(400);
+                  return;
+              }
+              if (doc === null) {
+                  console.log("doc is null");
+                  var emptyDoc = {};
+                  //just for testing
+                  res.status(200).json(emptyDoc);
+                  return;
+              }
+              console.log(doc);
+              res.status(200).json(doc);
+          });
+  };
 
-module.exports.deleteMess = function(req, res) {
-    Message.remove({}, function(err){
-        if(err){
-          console.log(err);
-          res.status(400);
+  module.exports.deleteMess = function(req, res) {
+      Message.remove({}, function(err) {
+          if (err) {
+              console.log(err);
+              res.status(400);
+              return;
+          }
+          res.status(200);
           return;
-        }
-        res.status(200);
-        return;
-    });
-};
+      });
+  };
