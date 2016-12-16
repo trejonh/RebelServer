b09a061131d5f1e8983208ed32d775bcca4f2f26
TreJon House <houset@msoe.edu>
2016-12-16 09:43:04 -0600

    last update before branch merge

--- src/client/app/scripts/controllers/headerCtrl.js
+++ src/client/app/scripts/controllers/headerCtrl.js
@@ -3,11 +3,12 @@
 angular.module('clientApp')
   .controller('HeaderCtrl', function($scope, $location, $interval, authentication) {
     var header = this; // jshint ignore:line
-    $scope.loggedIn = !authentication.isLoggedIn();//true == hidden, false==visible
+    $scope.loggedIn = !authentication.isLoggedIn(); //true == hidden, false==visible
     $interval(function() {
       $scope.loggedIn = !authentication.isLoggedIn();
     }, 5000);
     $scope.logout = function() {
+      $scope.loggedIn = !authentication.isLoggedIn(); //true == hidden, false==visible
       authentication.logout();
       $location.path("/");
     };
