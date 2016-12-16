34b16df8a5e5d865b902b2e1b2f26f5621a7dfcb
TreJon House <houset@msoe.edu>
2016-12-16 09:30:07 -0600

    tinkering

--- src/client/app/scripts/controllers/footerCtrl.js
+++ src/client/app/scripts/controllers/footerCtrl.js
@@ -3,14 +3,9 @@
 angular.module('clientApp')
   .controller('HeaderCtrl', function($scope, $location, $interval, authentication) {
     var header = this; // jshint ignore:line
-    console.log(header);
-    console.log($scope);
-    var checkedIfLoggedIn = $interval(function() { //jshint ignore:line
-      if (authentication.isLoggedIn()) {
-        $scope.loggedIn = false;
-      } else {
-        $scope.loggedIn = true;
-      }
+    $scope.loggedIn = authentication.isLoggedIn();
+    $interval(function() {
+      $scope.loggedIn = authentication.isLoggedIn();
     }, 5000);
     $scope.logout = function() {
       authentication.logout();
