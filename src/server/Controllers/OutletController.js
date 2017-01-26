7be0ac204170e08c18c21fd19d71cb7379e33f82
TreJon House <houset@msoe.edu>
2017-01-26 11:30:59 -0600

    trying to schedule a task now

--- src/server/Controllers/OutletController.js
+++ src/server/Controllers/OutletController.js
@@ -221,14 +221,13 @@
       if (req.body.isOn === 1) {
           triggerPower(request.body.deviceID, request.body._id, request.body.outletNumber, request.body.access_token, "turnOn", function() {
               res.status(200).end();
-              updateTasks(request);
           });
       } else {
           triggerPower(request.body.deviceID, request.body._id, request.body.outletNumber, request.body.access_token, "turnOff", function() {
               res.status(200).end();
-              updateTasks(request);
           });
       }
+      updateTasks(request);
   };
 
   function updateTasks(req) {
